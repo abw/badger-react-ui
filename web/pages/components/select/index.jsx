38297aa43a415e319738e8aca6c0c1d9c80b3bf6
Andy Wardley <abw@wardley.org>
2023-12-01 18:48:21 +0000

    Docs

--- web/pages/components/select/index.jsx
+++ web/pages/components/select/index.jsx
@@ -13,6 +13,50 @@ const SelectExamples = () =>
       <Options/>
       <DisplayOption/>
       <DisplayValue/>
+      <h2>Other Properties</h2>
+      <p>
+        These are implemented but the documentation is TODO.
+      </p>
+
+      <h3>General</h3>
+      <ul>
+        <li>disabled</li>
+        <li>placeholder</li>
+      </ul>
+
+      <h3>CSS Classes</h3>
+      <ul>
+        <li>placeholderClass</li>
+        <li>inputClass</li>
+        <li>selectClass</li>
+        <li>selectingClass</li>
+        <li>optionsClass</li>
+        <li>optionClass</li>
+        <li>noOptionsClass</li>
+        <li>activeClass</li>
+      </ul>
+
+      <h3>Event Handlers</h3>
+      <ul>
+        <li>onLoad</li>
+        <li>onUnload</li>
+        <li>onFocus</li>
+        <li>onBlur</li>
+        <li>onClick</li>
+        <li>onOpen</li>
+        <li>onClose</li>
+        <li>onSelect</li>
+      </ul>
+
+      <h3>Components</h3>
+      <ul>
+        <li>Content</li>
+        <li>Input</li>
+        <li>Options</li>
+        <li>NoOptions</li>
+        <li>Option</li>
+      </ul>
+
     </Sections>
   </div>
 
