fc0c692259c0312526645586efefe77bfed42f4f
Andy Wardley <abw@wardley.org>
2024-01-21 10:10:56 +0000

    Deleted old datatable controls

--- src/components/Datatable/Content.jsx
+++ src/components/Datatable/Content.jsx
@@ -3,7 +3,6 @@ import Context           from './Context.js'
 import DatatableHeader   from './Header.jsx'
 import DatatableBody     from './Body.jsx'
 import DatatableFooter   from './Footer.jsx'
-import DatatableControls from './Controls.jsx'
 import { classes } from '@/src/utils/classes.js'
 
 const Content = ({
@@ -12,13 +11,11 @@ const Content = ({
   Header=DatatableHeader,
   Body=DatatableBody,
   Footer=DatatableFooter,
-  Controls=DatatableControls,
 }) =>
   <section className={classes(className, color)}>
     <Header/>
     <Body/>
     <Footer/>
-    <Controls/>
   </section>
 
 export const DatatableContent = Context.Consumer(Content)
