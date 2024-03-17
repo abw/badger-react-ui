24bea58fee76a04984a0489411a6546f1d33e699
Andy Wardley <abw@wardley.org>
2024-03-17 13:43:24 +0000

    Changed select form component to forward all props

--- src/components/Form/Input/Select.jsx
+++ src/components/Form/Input/Select.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import UISelect from '@/components/Select/Select.jsx'
 import { useField } from '@abw/badger-form'
-import { extract, identity } from '@abw/badger-utils'
+import { identity } from '@abw/badger-utils'
 
 const Select = ({
   field=useField(),
   selectValue=field.selectValue || identity
 }) =>
   <UISelect
+    {...field}
+    onSelect={option => field.onChange(selectValue(option))}
+    /* First approach - selectively extracting options.  This might be wiser
+    ** in the long run to prevent any conflicts, but the list below is
+    ** incomplete.  e.g. missing displayOption, displayValue, searchOptions,
+    ** noOptions, openOnHover, closeOnBlur, closeDelay, event handlers, CSS
+    ** classes, etc.
     { ...extract(
       field,
       'value options search disabled placeholder onFocus onBlur onClick onOpen onClose'
     )}
-    onSelect={option => field.onChange(selectValue(option))}
+    */
   />
 
 export default Select
