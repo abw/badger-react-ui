ee557f9817064fd3b0631aeb077e2ee9ff8cd28e
Andy Wardley <abw@wardley.org>
2024-03-17 13:12:44 +0000

    Fixed sortable examples that were sharing data

--- web/pages/components/sortable/_examples/Sortable/Component.jsx
+++ web/pages/components/sortable/_examples/Sortable/Component.jsx
@@ -3,7 +3,17 @@ import { Sortable, Button } from '@/src/index.jsx'
 /* START */
 import React from 'react'
 // PRETEND: import { Sortable, Button } from '@abw/badger-react-ui
-import { animals } from '../data.js'
+
+export const animals = [
+  { id: 100, animal: 'Alan Aardvark' },
+  { id: 101, animal: 'Brian Badger' },
+  { id: 102, animal: 'Colin Camel' },
+  { id: 103, animal: 'David Dog' },
+  { id: 104, animal: 'Edward Elephant' },
+  { id: 105, animal: 'Frank Ferret' },
+  { id: 106, animal: 'Gerald Giraffe' },
+  { id: 107, animal: 'Hector Hedgehog' },
+]
 
 const SortableExample = () => {
   const [changed, setChanged] = React.useState(false)
--- web/pages/components/sortable/_examples/Vertical/Component.jsx
+++ web/pages/components/sortable/_examples/Vertical/Component.jsx
@@ -3,7 +3,17 @@ import { VerticalSort, Button } from '@/src/index.jsx'
 /* START */
 import React from 'react'
 // PRETEND: import { VerticalSort, Button } from '@abw/badger-react-ui
-import { animals } from '../data.js'
+
+export const animals = [
+  { id: 100, animal: 'Alan Aardvark' },
+  { id: 101, animal: 'Brian Badger' },
+  { id: 102, animal: 'Colin Camel' },
+  { id: 103, animal: 'David Dog' },
+  { id: 104, animal: 'Edward Elephant' },
+  { id: 105, animal: 'Frank Ferret' },
+  { id: 106, animal: 'Gerald Giraffe' },
+  { id: 107, animal: 'Hector Hedgehog' },
+]
 
 const VerticalSortExample = () => {
   const [changed, setChanged] = React.useState(false)
