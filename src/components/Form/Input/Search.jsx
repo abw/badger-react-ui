e1d269069abae78c51db1ed691344996d77ea070
Andy Wardley <abw@wardley.org>
2024-03-17 13:43:58 +0000

    Form search component

---
+++ src/components/Form/Input/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UISearch from '@/components/Search/Search.jsx'
+import { useField } from '@abw/badger-form'
+import { identity } from '@abw/badger-utils'
+
+const Search = ({
+  field=useField(),
+  selectValue=field.selectValue || identity
+}) =>
+  <UISearch
+    {...field}
+    onSelect={option => field.onChange(selectValue(option))}
+  />
+
+export default Search
