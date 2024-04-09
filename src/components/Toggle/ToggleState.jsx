4236dcb48c211562dc5a16260ad99b24da7c7690
Andy Wardley <abw@wardley.org>
2024-04-09 10:09:25 +0100

    Toggle component

--- src/components/Toggle/ToggleState.jsx
+++ src/components/Toggle/ToggleState.jsx
@@ -1,39 +1,9 @@
-import React, { useState } from 'react'
-import { Storage } from '@/src/index.jsx'
-import { doNothing } from '@abw/badger-utils'
-import Toggle, { defaultToggleOptions, selectedToggleIndex } from './Toggle.jsx'
+import React from 'react'
+import useToggle from './useToggle.jsx'
 
-const ToggleState = ({
-  selected=0,
-  findSelectedIndex=selectedToggleIndex,
-  options=defaultToggleOptions,
-  onSelect=doNothing,
-  storageKey,
-  storageItem='selected',
-  ...props
-} = { }) => {
-  const selectedIndex = findSelectedIndex(options, selected)
-  const store = storageKey && Storage(storageKey)
-  const [index, setIndex] = useState(
-    store
-      ? store.get(storageItem) ?? selectedIndex
-      : selectedIndex
-  )
-  const selectOption = n => {
-    if (store) {
-      store.set(storageItem, n)
-    }
-    setIndex(n)
-    onSelect(options[n], n)
-  }
-  return (
-    <Toggle
-      options={options}
-      selected={index}
-      onSelect={ (option,n) => selectOption(n) }
-      {...props}
-    />
-  )
+const ToggleState = props => {
+  const [ , Toggle] = useToggle(props)
+  return <Toggle/>
 }
 
 export default ToggleState
