a0f8304cb22951376f560eb73d7b92d3cc9f4063
Andy Wardley <abw@wardley.org>
2024-04-12 09:58:45 +0100

    Added some jsdoc to Table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -4,6 +4,28 @@ import TableBody from './Body.jsx'
 import TableFoot from './Foot.jsx'
 import { borderClass, classes, colorClass, radiusClass, shadowClass } from '@/src/utils/classes.js'
 
+/**
+ * Generate an HTML table.
+ *
+ * @param {string} className A CSS class
+ * @param {number} border Border width
+ * @param {string} color The name of a color range
+ * @param {integer} radius The border radius in multiples of 0.25rem
+ * @param {integer} shadow A shadow depth
+ * @param {string} size A CSS size class, e.g. large, small, etc.
+ * @param {boolean} lined Add the lined CSS class
+ * @param {boolean} celled Add the celled CSS class
+ * @param {boolean} shaded Add the shaded CSS class
+ * @param {boolean} striped Add the striped CSS class
+ * @param {boolean} wide Add the wide CSS class
+ * @param {boolean} compressed Add the compressed CSS class
+ * @param {boolean} expanded Add the expanded CSS class
+ * @param {Array} headings An array of table headings
+ * @param {Array} rows An array of table rows
+ * @param {Array} footings An array of table footers
+ * @returns {ReactNode} A React element that renders a table.
+ */
+
 const Table = ({
   className,
   border,
