dd3301d9f9676989440c3fe13fa344c16e8fe286
Andy Wardley <abw@wardley.org>
2024-05-04 09:23:06 +0100

    Updates to useContainer and useFloating

--- src/hooks/useContainer.jsx
+++ src/hooks/useContainer.jsx
@@ -4,6 +4,8 @@ import { widthBreakpoint } from '@/src/utils/breakpoint.js'
 export const useContainer = (options={}) => {
   const [width, setWidth] = useState(false)
   const [height, setHeight] = useState(false)
+  const [boxWidth, setBoxWidth] = useState(false)
+  const [boxHeight, setBoxHeight] = useState(false)
   const [breakpoint, setBreakpoint] = useState(false)
   const ref = useRef()
 
@@ -12,14 +14,21 @@ export const useContainer = (options={}) => {
       if (!ref.current) return
       const resizeObserver = new ResizeObserver(
         entries => {
-          const entry = entries[0]
-          const rect  = entry.contentRect
+          const entry  = entries[0]
+          const rect   = entry.contentRect
+          const border = entry.borderBoxSize[0]
           if (rect) {
             const width = Math.round(rect.width)
             const height = Math.round(rect.height)
             setWidth(width)
             setHeight(height)
             setBreakpoint(widthBreakpoint(width, options))
+          }
+          if (border) {
+            const width = Math.round(border.inlineSize)
+            const height = Math.round(border.blockSize)
+            setBoxWidth(width)
+            setBoxHeight(height)
           }
         }
       )
@@ -29,7 +38,7 @@ export const useContainer = (options={}) => {
     []
   )
 
-  return { ref, width, height, breakpoint }
+  return { ref, width, height, boxWidth, boxHeight, breakpoint }
 }
 
 export default useContainer
--- src/hooks/useFloating.jsx
+++ src/hooks/useFloating.jsx
@@ -1,14 +1,50 @@
-import { useFloating as useFUI, offset as fuiOffset } from '@floating-ui/react'
+import {
+  useFloating as useFUI,
+  offset as fuiOffset,
+  size
+} from '@floating-ui/react'
+
+const floatingMatch = fn => size({
+  apply({ rects, elements }) {
+    Object.assign(
+      elements.floating.style,
+      fn(rects.reference)
+    )
+  }
+})
+
+const floatingMatchWidth = floatingMatch(
+  ref => ({ width: `${ref.width}px` })
+)
+
+const floatingMatchHeight = floatingMatch(
+  ref => ({ height: `${ref.height}px` })
+)
 
 export const useFloating = ({
   offset=0,
   placement,
+  matchWidth,
+  matchHeight,
   right
-}) => useFUI({
-  middleware: [fuiOffset(parseInt(offset))],
-  placement: placement || (right
-    ? 'bottom-end'
-    : 'bottom-start')
-})
+}) => {
+  const middleware = [
+    fuiOffset(parseInt(offset))
+  ]
+  if (matchWidth) {
+    middleware.push(floatingMatchWidth)
+  }
+  if (matchHeight) {
+    middleware.push(floatingMatchHeight)
+  }
+  return useFUI({
+    middleware,
+    placement: placement || (right
+      ? 'bottom-end'
+      : 'bottom-start')
+  })
+}
+
+
 
 export default useFloating
