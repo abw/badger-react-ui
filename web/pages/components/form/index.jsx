87ae85491c56942d7b5b09e1588e1fdc2d79d0d3
Andy Wardley <abw@wardley.org>
2024-05-29 08:26:55 +0100

    Added (and documented) some missing components from badger-form

--- web/pages/components/form/index.jsx
+++ web/pages/components/form/index.jsx
@@ -1,5 +1,6 @@
 import React        from 'react'
 import Example      from '@/site/Example.jsx'
+import DescTable    from '@/site/DescTable.jsx'
 import Heading      from '@/page/Heading.jsx'
 import Sections     from '@/page/Sections.jsx'
 // import Section      from '@/page/Section.jsx'
@@ -26,6 +27,43 @@ const FormExamples = () =>
       caption="Form"
       // expand
     />
+    <h2>Components</h2>
+    <p>
+      The table below lists the component that are exported from{' '}
+      <a href="https://abw.github.io/badger-form/">Badger Form</a>.
+      In some cases the names have been changed to{' '}
+      <strike>protect the innocent</strike> disambiguate and avoid conflicts.
+    </p>
+    <DescTable
+      items={[
+        ['Form',          'Main `Form` component, as per badger-form'],
+        ['Field',         'Form `Field` component, as per badger-form'],
+        ['Fields',        'Form `Fields` component, as per badger-form'],
+        ['Fieldset',      'Form `Fieldset` component, as per badger-form'],
+        ['Errors',        'Form `Errors` component, as per badger-form'],
+        ['Status',        'Form `Status` component, as per badger-form'],
+        ['Debug',         'Form `Debug` component, as per badger-form'],
+        ['Layout',        'Field `Layout` component, as per badger-form'],
+        ['Label',         'Field `Label` component, as per badger-form'],
+        ['Input',         'Field `Input` component, as per badger-form'],
+        ['Message',       'Field `Message` component, as per badger-form'],
+        ['CheckboxInput', '`Checkbox` input component from badger-form'],
+        ['HiddenInput',   '`Hidden` input component from badger-form'],
+        ['RadioInput',    '`Radio` input component from badger-form'],
+        ['SelectInput',   '`Select` input component from badger-form'],
+        ['TextInput',     '`Text` input component from badger-form'],
+        ['TextAreaInput', '`TextArea` input component from badger-form'],
+        ['useForm',       'Hook to access form context, as per badger-form'],
+        ['useField',      'Hook to access field context, as per badger-form'],
+        ['UseForm',       'Component wrapper to access form context, as per badger-form'],
+        ['UseField',      'Component wrapper to access field context, as per badger-form'],
+        ['fieldClass',    'Utility function to generate field CSS class'],
+        ['inputClasses',  'Utility function to generate input CSS class'],
+        ['statusClasses', 'Utility function to generate status CSS class'],
+        ['addInputType',  'Utility function to add new input types'],
+      ]}
+    />
+
     <p>
       There are a few enhancements.  First, the buttons are rendered using
       the <Link to="/components/button" text="Button"/> component, allowing
--- web/site/DescTable.jsx
+++ web/site/DescTable.jsx
@@ -1,3 +1,4 @@
+import { hasValue } from '@abw/badger-utils'
 import React from 'react'
 
 const DescTable = ({items}) =>
@@ -13,7 +14,14 @@ const DescTable = ({items}) =>
 const DescRow = ({name, value}) =>
   <tr>
     <td className="font-mono">{name}</td>
-    <td>{value}</td>
+    <td dangerouslySetInnerHTML={{__html: codify(value)}}/>
   </tr>
+
+// function to convert `backticks` to <code>backticks</code>
+export function codify(text, codeClass='') {
+  return hasValue(text)
+    ? text.replace(/`([^`]*)`/g, `<code class=${codeClass}>$1</code>`)
+    : ''
+}
 
 export default DescTable
