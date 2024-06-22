df5155a2832c5de748a8f7c16e9c0bbac22f639b
Andy Wardley <abw@wardley.org>
2024-06-22 16:36:22 +0100

    Themed Icon and Icons

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Icon as BadgerIcon } from '@abw/badger-icon'
 import { sizeColorProps } from '@/src/utils/classes.js'
+import { Themed }     from '@/src/Theme.jsx'
 
 const Icon = (props) =>
   <BadgerIcon
     {...sizeColorProps(props)}
   />
 
-
-export default Icon
+export default Themed(Icon, 'Icon')
