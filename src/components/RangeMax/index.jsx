3c9294606fbeba2f5ca118b796c50b4379907461
Andy Wardley <abw@wardley.org>
2024-06-29 17:23:37 +0100

    Range export fix

--- src/components/RangeMax/index.jsx
+++ src/components/RangeMax/index.jsx
@@ -1,4 +1,5 @@
 export { default as RangeMax        } from './RangeMax.jsx'
 export { default as RangeMaxValues  } from './Values.jsx'
 export { default as RangeMaxThumbs  } from './Thumbs.jsx'
-export { default as RangeInputs     } from './Inputs.jsx'
+export { default as RangeMaxInputs  } from './Inputs.jsx'
+export * from './Utils.js'
