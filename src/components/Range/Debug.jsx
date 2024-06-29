0096bfff209c2804ff99a72c6bce0d65893659e0
Andy Wardley <abw@wardley.org>
2024-06-29 16:50:54 +0100

    Range debug component

--- src/components/Range/Debug.jsx
+++ src/components/Range/Debug.jsx
@@ -3,14 +3,32 @@ import Context from './Context.js'
 
 const Debug = ({
   min, max,
-  value,
-  percent,
-  step,
-  steps
+  minValue, maxValue,
+  minNormal, maxNormal,
+  minPercent, maxPercent,
+  step, steps
 }) =>
-  <div>
-    {min} to {max} step:{step} steps:{steps} - value:{value} ({percent}%)
-  </div>
+  <table className="celled shaded grey smaller">
+    <tbody>
+      {/* {min} to {max} step:{step} steps:{steps} - value:{value} ({percent}%) */}
+      {debugRow('minValue', minValue)}
+      {debugRow('maxValue', maxValue)}
+      {debugRow('minNormal', minNormal)}
+      {debugRow('maxNormal', maxNormal)}
+      {debugRow('minPercent', minPercent)}
+      {debugRow('maxPercent', maxPercent)}
+      {debugRow('min', min)}
+      {debugRow('max', max)}
+      {debugRow('step', step)}
+      {debugRow('steps', steps)}
+    </tbody>
+  </table>
+
+const debugRow = (key, value) =>
+  <tr>
+    <th className="font-mono split-3">{key}</th>
+    <td>{value}</td>
+  </tr>
 
 export const RangeDebug = Context.Consumer(Debug)
 export default RangeDebug
