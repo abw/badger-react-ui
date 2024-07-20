60d560355ca4984710b52849bd0579a151e84ebc
Andy Wardley <abw@wardley.org>
2024-07-20 09:39:36 +0100

    Dropdown tests

---
+++ test/components/Dropdown/content.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { test, expect } from 'vitest'
+import { render, act } from '@testing-library/react'
+import { Dropdown } from '@/src/index.jsx'
+
+const DropdownExample = () =>
+  <Dropdown
+    text='Dropdown'
+    content='Hello World!'
+  />
+
+test(
+  'Dropdown with content',
+  async () => {
+    const user = userEvent.setup()
+    const { container } = render(
+      <DropdownExample/>
+    )
+
+    const dropdown = container.querySelector('div.dropdown')
+    const trigger = dropdown.querySelector('div.trigger')
+    const nobody = dropdown.querySelector('div.body')
+    expect(nobody).toBeNull()
+
+    await act( () => user.click(trigger) )
+    const somebody = dropdown.querySelector('div.body')
+    expect(somebody).toBeTruthy()
+    expect(somebody).toHaveTextContent('Hello World!')
+  }
+)
