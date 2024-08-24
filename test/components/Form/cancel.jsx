5980fdb60fce806f8eaffe9759deef3314534d73
Andy Wardley <abw@wardley.org>
2024-08-24 14:01:24 +0100

    Form Cancel and CancelSubmit tests

---
+++ test/components/Form/cancel.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import { test, expect } from 'vitest'
+import { render, screen, act } from '@testing-library/react'
+import { Form, Cancel } from '@/src/index.jsx'
+
+const FormExample = () => {
+  const [cancel, setCancel] = React.useState('Not clicked')
+  return (
+    <>
+      <Form
+        className="relative"
+      >
+        <Cancel
+          id="cancel-button"
+          text="Click to Cancel"
+          onClick={() => setCancel('Cancel was clicked')}
+        />
+      </Form>
+      <div data-testid="clicked">{cancel}</div>
+    </>
+  )
+}
+
+test(
+  'form cancel',
+  async () => {
+    const user = userEvent.setup()
+    const { container } = render(
+      <FormExample/>
+    )
+    const button = container.querySelector('#cancel-button')
+    const clicked = screen.getByTestId('clicked')
+
+    expect(button).toHaveTextContent('Click to Cancel')
+    expect(clicked).toHaveTextContent('Not clicked')
+
+    // click on the cancel button
+    await act( () => user.click(button) )
+    expect(clicked).toHaveTextContent('Cancel was clicked')
+  }
+)
