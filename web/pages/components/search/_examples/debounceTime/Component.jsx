6a73f0c3da407bce1b00f259588ca7034156f00a
Andy Wardley <abw@wardley.org>
2024-12-09 09:43:42 +0000

    Added documentation for Search minLength and debounceTime

---
+++ web/pages/components/search/_examples/debounceTime/Component.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Search } from '@/src/index.jsx'
+
+const animals = [
+  'Alan Aardvark',
+  'Amy Aardvark',
+  'Anne Aardvark',
+  'Bart Badger',
+  'Beatrice Badger',
+  'Belinda Badger',
+  'Ben Badger',
+  'Bert Badger',
+  'Betty Badger',
+  'Bobby Badger',
+  'Brenda Badger',
+  'Brian Badger',
+  'Brook Badger',
+  'Buzz Badger',
+]
+
+const DebounceTimeExample = () => {
+  const onSearch = input =>
+    animals.filter(
+      animal =>
+        animal
+          .toLowerCase()
+          .indexOf(input.toLowerCase()) !== -1
+    )
+
+  return (
+    /* START */
+    <Search
+      debounceTime="100"
+      onSearch={onSearch}
+    />
+    /* END */
+  )
+}
+
+export default DebounceTimeExample
---
+++ web/pages/components/search/_examples/minLength/Component.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Search } from '@/src/index.jsx'
+
+const animals = [
+  'Alan Aardvark',
+  'Amy Aardvark',
+  'Anne Aardvark',
+  'Bart Badger',
+  'Beatrice Badger',
+  'Belinda Badger',
+  'Ben Badger',
+  'Bert Badger',
+  'Betty Badger',
+  'Bobby Badger',
+  'Brenda Badger',
+  'Brian Badger',
+  'Brook Badger',
+  'Buzz Badger',
+]
+
+const MinLengthExample = () => {
+  const onSearch = input =>
+    animals.filter(
+      animal =>
+        animal
+          .toLowerCase()
+          .indexOf(input.toLowerCase()) !== -1
+    )
+
+  return (
+    /* START */
+    <Search
+      minLength="4"
+      onSearch={onSearch}
+    />
+    /* END */
+  )
+}
+
+export default MinLengthExample
