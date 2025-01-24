8dc639d8835c3db12fcb0e700f3d4a05f29616e4
Andy Wardley <abw@wardley.org>
2025-01-24 16:03:56 +0000

    First attempt at adding generic types to useComplexState

--- src/hooks/useComplexState.tsx
+++ src/hooks/useComplexState.tsx
@@ -3,22 +3,30 @@ import { maybeFunction } from '@abw/badger-utils'
 import { capitalFirstLetter } from '@/src/utils/text'
 
 export type UseComplexStateValues = Record<string, unknown>
-export type UseComplexStateOptions = {
-  onChange?: (values: UseComplexStateValues) => UseComplexStateValues
+export type UseComplexStateOptions<Values> = {
+  onChange?: UseComplexStateOnChange<Values>
   convertCase?: (word: string) => string
   setterNamer?: (key: string) => string
 }
+export type UseComplexStateOnChange<Values> = (values: Values) => Values
 
-export const useComplexState = (
-  values: UseComplexStateValues,
-  options: UseComplexStateOptions={ }
-) => {
+type ValuesSetters<Values> = {
+    [Property in keyof Values as `set${Capitalize<string & Property>}`]: (value: Values[Property]) => void
+};
+
+export const useComplexState = <
+  Values extends UseComplexStateValues = UseComplexStateValues,
+  Setters = ValuesSetters<Values>
+>(
+  values: Values,
+  options: UseComplexStateOptions<Values>={ }
+) : [Values, Setters] => {
   const {
     onChange,
     convertCase = capitalFirstLetter,
     setterNamer = key => `set${convertCase(key)}`
   } = options
-  const [state, setState] = useState(
+  const [state, setState] = useState<Values>(
     onChange
       ? onChange(values)
       : values
@@ -27,6 +35,7 @@ export const useComplexState = (
   const setters = Object.keys(values).reduce(
     (setters, key) => {
       const setter = setterNamer(key)
+      // @ts-expect-error - FIXME!
       setters[setter] = (value: unknown) => setState(
         oldState => {
           const newState = {
@@ -40,7 +49,7 @@ export const useComplexState = (
       )
       return setters
     },
-    { } as UseComplexStateValues
+    { } as Setters
   )
   return [state, setters]
 }
